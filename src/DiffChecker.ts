9de4d945707d4ef685fdfd893f3724863475a117
Kunal Patel <kunal.patel@visory.com.au>
2024-07-05 00:31:50 +1000

    Revert,  add prettier rc

--- src/DiffChecker.ts
+++ src/DiffChecker.ts
@@ -1,180 +1,153 @@
 /* eslint-disable github/array-foreach */
-import * as core from '@actions/core'
-import {CoverageReport} from './Model/CoverageReport'
-import {DiffCoverageReport} from './Model/DiffCoverageReport'
-import {CoverageData} from './Model/CoverageData'
-import {DiffFileCoverageData} from './Model/DiffFileCoverageData'
-import {DiffCoverageData} from './Model/DiffCoverageData'
+import * as core from "@actions/core";
+import { CoverageReport } from "./Model/CoverageReport";
+import { DiffCoverageReport } from "./Model/DiffCoverageReport";
+import { CoverageData } from "./Model/CoverageData";
+import { DiffFileCoverageData } from "./Model/DiffFileCoverageData";
+import { DiffCoverageData } from "./Model/DiffCoverageData";
 
-const increasedCoverageIcon = ':green_circle:'
-const decreasedCoverageIcon = ':red_circle:'
-const newCoverageIcon = ':sparkles: :new:'
-const removedCoverageIcon = ':x:'
+const increasedCoverageIcon = ":green_circle:";
+const decreasedCoverageIcon = ":red_circle:";
+const newCoverageIcon = ":sparkles: :new:";
+const removedCoverageIcon = ":x:";
 
 export class DiffChecker {
-  private diffCoverageReport: DiffCoverageReport = {}
-  constructor(
-    coverageReportNew: CoverageReport,
-    coverageReportOld: CoverageReport
-  ) {
-    const reportNewKeys = Object.keys(coverageReportNew)
-    const reportOldKeys = Object.keys(coverageReportOld)
-    const reportKeys = new Set([...reportNewKeys, ...reportOldKeys])
+    private diffCoverageReport: DiffCoverageReport = {};
+    constructor(coverageReportNew: CoverageReport, coverageReportOld: CoverageReport) {
+        const reportNewKeys = Object.keys(coverageReportNew);
+        const reportOldKeys = Object.keys(coverageReportOld);
+        const reportKeys = new Set([...reportNewKeys, ...reportOldKeys]);
 
-    for (const filePath of reportKeys) {
-      this.diffCoverageReport[filePath] = {
-        branches: {
-          newPct: this.getPercentage(coverageReportNew[filePath]?.branches),
-          oldPct: this.getPercentage(coverageReportOld[filePath]?.branches)
-        },
-        statements: {
-          newPct: this.getPercentage(coverageReportNew[filePath]?.statements),
-          oldPct: this.getPercentage(coverageReportOld[filePath]?.statements)
-        },
-        lines: {
-          newPct: this.getPercentage(coverageReportNew[filePath]?.lines),
-          oldPct: this.getPercentage(coverageReportOld[filePath]?.lines)
-        },
-        functions: {
-          newPct: this.getPercentage(coverageReportNew[filePath]?.functions),
-          oldPct: this.getPercentage(coverageReportOld[filePath]?.functions)
+        for (const filePath of reportKeys) {
+            this.diffCoverageReport[filePath] = {
+                branches: {
+                    newPct: this.getPercentage(coverageReportNew[filePath]?.branches),
+                    oldPct: this.getPercentage(coverageReportOld[filePath]?.branches),
+                },
+                statements: {
+                    newPct: this.getPercentage(coverageReportNew[filePath]?.statements),
+                    oldPct: this.getPercentage(coverageReportOld[filePath]?.statements),
+                },
+                lines: {
+                    newPct: this.getPercentage(coverageReportNew[filePath]?.lines),
+                    oldPct: this.getPercentage(coverageReportOld[filePath]?.lines),
+                },
+                functions: {
+                    newPct: this.getPercentage(coverageReportNew[filePath]?.functions),
+                    oldPct: this.getPercentage(coverageReportOld[filePath]?.functions),
+                },
+            };
         }
-      }
     }
-  }
 
-  getCoverageDetails(diffOnly: boolean, currentDirectory: string): string[] {
-    const keys = Object.keys(this.diffCoverageReport)
-    const returnStrings: string[] = []
-    for (const key of keys) {
-      if (this.compareCoverageValues(this.diffCoverageReport[key]) !== 0) {
-        returnStrings.push(
-          this.createDiffLine(
-            key.replace(currentDirectory, ''),
-            this.diffCoverageReport[key]
-          )
-        )
-      } else {
-        if (!diffOnly) {
-          returnStrings.push(
-            `${key.replace(currentDirectory, '')} | ${
-              this.diffCoverageReport[key].statements.newPct
-            } | ${this.diffCoverageReport[key].branches.newPct} | ${
-              this.diffCoverageReport[key].functions.newPct
-            } | ${this.diffCoverageReport[key].lines.newPct}`
-          )
+    getCoverageDetails(diffOnly: boolean, currentDirectory: string): string[] {
+        const keys = Object.keys(this.diffCoverageReport);
+        const returnStrings: string[] = [];
+        for (const key of keys) {
+            if (this.compareCoverageValues(this.diffCoverageReport[key]) !== 0) {
+                returnStrings.push(
+                    this.createDiffLine(key.replace(currentDirectory, ""), this.diffCoverageReport[key]),
+                );
+            } else {
+                if (!diffOnly) {
+                    returnStrings.push(
+                        `${key.replace(currentDirectory, "")} | ${this.diffCoverageReport[key].statements.newPct} | ${
+                            this.diffCoverageReport[key].branches.newPct
+                        } | ${this.diffCoverageReport[key].functions.newPct} | ${
+                            this.diffCoverageReport[key].lines.newPct
+                        }`,
+                    );
+                }
+            }
         }
-      }
+        return returnStrings;
     }
-    return returnStrings
-  }
 
-  checkIfTestCoverageFallsBelowDelta(
-    delta: number,
-    totalDelta: number | null
-  ): boolean {
-    const files = Object.keys(this.diffCoverageReport)
-    for (const file of files) {
-      const diffCoverageData = this.diffCoverageReport[file]
-      const keys: ('lines' | 'statements' | 'branches' | 'functions')[] = <
-        ('lines' | 'statements' | 'branches' | 'functions')[]
-      >Object.keys(diffCoverageData)
-      // No new coverage found so that means we deleted a file coverage
-      const fileRemovedCoverage = Object.values(diffCoverageData).every(
-        coverageData => coverageData.newPct === 0
-      )
-      if (fileRemovedCoverage) {
-        core.info(
-          `${file} : deleted or renamed and is not considered for coverage diff.`
-        )
-        // since the file is deleted don't include in delta calculation
-        continue
-      }
-      for (const key of keys) {
-        if (diffCoverageData[key].oldPct !== diffCoverageData[key].newPct) {
-          const deltaToCompareWith =
-            file === 'total' && totalDelta !== null ? totalDelta : delta
-          if (
-            -this.getPercentageDiff(diffCoverageData[key]) > deltaToCompareWith
-          ) {
-            const percentageDiff = this.getPercentageDiff(diffCoverageData[key])
-            core.info(
-              `percentage Diff: ${percentageDiff} is greater than delta for ${file}`
-            )
-            return true
-          }
+    checkIfTestCoverageFallsBelowDelta(delta: number, totalDelta: number | null): boolean {
+        const files = Object.keys(this.diffCoverageReport);
+        for (const file of files) {
+            const diffCoverageData = this.diffCoverageReport[file];
+            const keys: ("lines" | "statements" | "branches" | "functions")[] = <
+                ("lines" | "statements" | "branches" | "functions")[]
+            >Object.keys(diffCoverageData);
+            // No new coverage found so that means we deleted a file coverage
+            const fileRemovedCoverage = Object.values(diffCoverageData).every(
+                (coverageData) => coverageData.newPct === 0,
+            );
+            if (fileRemovedCoverage) {
+                core.info(`${file} : deleted or renamed and is not considered for coverage diff.`);
+                // since the file is deleted don't include in delta calculation
+                continue;
+            }
+            for (const key of keys) {
+                if (diffCoverageData[key].oldPct !== diffCoverageData[key].newPct) {
+                    const deltaToCompareWith = file === "total" && totalDelta !== null ? totalDelta : delta;
+                    if (-this.getPercentageDiff(diffCoverageData[key]) > deltaToCompareWith) {
+                        const percentageDiff = this.getPercentageDiff(diffCoverageData[key]);
+                        core.info(`percentage Diff: ${percentageDiff} is greater than delta for ${file}`);
+                        return true;
+                    }
+                }
+            }
         }
-      }
+
+        return false;
     }
 
-    return false
-  }
-
-  private createDiffLine(
-    name: string,
-    diffFileCoverageData: DiffFileCoverageData
-  ): string {
-    // No old coverage found so that means we added a new file coverage
-    const fileNewCoverage = Object.values(diffFileCoverageData).every(
-      coverageData => coverageData.oldPct === 0
-    )
-    // No new coverage found so that means we deleted a file coverage
-    const fileRemovedCoverage = Object.values(diffFileCoverageData).every(
-      coverageData => coverageData.newPct === 0
-    )
-    if (fileNewCoverage) {
-      return ` ${newCoverageIcon} | **${name}** | **${diffFileCoverageData.statements.newPct}** | **${diffFileCoverageData.branches.newPct}** | **${diffFileCoverageData.functions.newPct}** | **${diffFileCoverageData.lines.newPct}**`
-    } else if (fileRemovedCoverage) {
-      return ` ${removedCoverageIcon} | ~~${name}~~ | ~~${diffFileCoverageData.statements.oldPct}~~ | ~~${diffFileCoverageData.branches.oldPct}~~ | ~~${diffFileCoverageData.functions.oldPct}~~ | ~~${diffFileCoverageData.lines.oldPct}~~`
+    private createDiffLine(name: string, diffFileCoverageData: DiffFileCoverageData): string {
+        // No old coverage found so that means we added a new file coverage
+        const fileNewCoverage = Object.values(diffFileCoverageData).every((coverageData) => coverageData.oldPct === 0);
+        // No new coverage found so that means we deleted a file coverage
+        const fileRemovedCoverage = Object.values(diffFileCoverageData).every(
+            (coverageData) => coverageData.newPct === 0,
+        );
+        if (fileNewCoverage) {
+            return ` ${newCoverageIcon} | **${name}** | **${diffFileCoverageData.statements.newPct}** | **${diffFileCoverageData.branches.newPct}** | **${diffFileCoverageData.functions.newPct}** | **${diffFileCoverageData.lines.newPct}**`;
+        } else if (fileRemovedCoverage) {
+            return ` ${removedCoverageIcon} | ~~${name}~~ | ~~${diffFileCoverageData.statements.oldPct}~~ | ~~${diffFileCoverageData.branches.oldPct}~~ | ~~${diffFileCoverageData.functions.oldPct}~~ | ~~${diffFileCoverageData.lines.oldPct}~~`;
+        }
+        // Coverage existed before so calculate the diff status
+        const statusIcon = this.getStatusIcon(diffFileCoverageData);
+        return ` ${statusIcon} | ${name} | ${diffFileCoverageData.statements.newPct} **(${this.getPercentageDiff(
+            diffFileCoverageData.statements,
+        )})** | ${diffFileCoverageData.branches.newPct} **(${this.getPercentageDiff(
+            diffFileCoverageData.branches,
+        )})** | ${diffFileCoverageData.functions.newPct} **(${this.getPercentageDiff(
+            diffFileCoverageData.functions,
+        )})** | ${diffFileCoverageData.lines.newPct} **(${this.getPercentageDiff(diffFileCoverageData.lines)})**`;
     }
-    // Coverage existed before so calculate the diff status
-    const statusIcon = this.getStatusIcon(diffFileCoverageData)
-    return ` ${statusIcon} | ${name} | ${
-      diffFileCoverageData.statements.newPct
-    } **(${this.getPercentageDiff(diffFileCoverageData.statements)})** | ${
-      diffFileCoverageData.branches.newPct
-    } **(${this.getPercentageDiff(diffFileCoverageData.branches)})** | ${
-      diffFileCoverageData.functions.newPct
-    } **(${this.getPercentageDiff(diffFileCoverageData.functions)})** | ${
-      diffFileCoverageData.lines.newPct
-    } **(${this.getPercentageDiff(diffFileCoverageData.lines)})**`
-  }
 
-  private compareCoverageValues(
-    diffCoverageData: DiffFileCoverageData
-  ): number {
-    const keys: ('lines' | 'statements' | 'branches' | 'functions')[] = <
-      ('lines' | 'statements' | 'branches' | 'functions')[]
-    >Object.keys(diffCoverageData)
-    for (const key of keys) {
-      if (diffCoverageData[key].oldPct !== diffCoverageData[key].newPct) {
-        return 1
-      }
+    private compareCoverageValues(diffCoverageData: DiffFileCoverageData): number {
+        const keys: ("lines" | "statements" | "branches" | "functions")[] = <
+            ("lines" | "statements" | "branches" | "functions")[]
+        >Object.keys(diffCoverageData);
+        for (const key of keys) {
+            if (diffCoverageData[key].oldPct !== diffCoverageData[key].newPct) {
+                return 1;
+            }
+        }
+        return 0;
     }
-    return 0
-  }
 
-  private getPercentage(coverageData: CoverageData): number {
-    return coverageData?.pct || 0
-  }
-
-  private getStatusIcon(
-    diffFileCoverageData: DiffFileCoverageData
-  ): ':green_circle:' | ':red_circle:' {
-    let overallDiff = 0
-    Object.values(diffFileCoverageData).forEach(coverageData => {
-      overallDiff = overallDiff + this.getPercentageDiff(coverageData)
-    })
-    if (overallDiff < 0) {
-      return decreasedCoverageIcon
+    private getPercentage(coverageData: CoverageData): number {
+        return coverageData?.pct || 0;
     }
-    return increasedCoverageIcon
-  }
 
-  private getPercentageDiff(diffData: DiffCoverageData): number {
-    // get diff
-    const diff = Number(diffData.newPct) - Number(diffData.oldPct)
-    // round off the diff to 2 decimal places
-    return Math.round((diff + Number.EPSILON) * 100) / 100
-  }
+    private getStatusIcon(diffFileCoverageData: DiffFileCoverageData): ":green_circle:" | ":red_circle:" {
+        let overallDiff = 0;
+        Object.values(diffFileCoverageData).forEach((coverageData) => {
+            overallDiff = overallDiff + this.getPercentageDiff(coverageData);
+        });
+        if (overallDiff < 0) {
+            return decreasedCoverageIcon;
+        }
+        return increasedCoverageIcon;
+    }
+
+    private getPercentageDiff(diffData: DiffCoverageData): number {
+        // get diff
+        const diff = Number(diffData.newPct) - Number(diffData.oldPct);
+        // round off the diff to 2 decimal places
+        return Math.round((diff + Number.EPSILON) * 100) / 100;
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,187 +1,127 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import * as cache from '@actions/cache'
-import {execSync} from 'child_process'
-import fs from 'fs'
-import {CoverageReport} from './Model/CoverageReport'
-import {DiffChecker} from './DiffChecker'
-import {Octokit} from '@octokit/core'
-import {PaginateInterface} from '@octokit/plugin-paginate-rest'
-import {RestEndpointMethods} from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { execSync } from "child_process";
+import fs from "fs";
+import { CoverageReport } from "./Model/CoverageReport";
+import { DiffChecker } from "./DiffChecker";
+import { Octokit } from "@octokit/core";
+import { PaginateInterface } from "@octokit/plugin-paginate-rest";
+import { RestEndpointMethods } from "@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types";
 
 async function run(): Promise<void> {
-  try {
-    const repoName = github.context.repo.repo
-    const repoOwner = github.context.repo.owner
-    const commitSha = github.context.sha
-    const githubToken = core.getInput('accessToken')
-    const fullCoverage = JSON.parse(core.getInput('fullCoverageDiff'))
-    const commandToRun = core.getInput('runCommand')
-    const commandAfterSwitch = core.getInput('afterSwitchCommand')
-    const afterSwitchCacheKey = core.getInput('afterSwitchCacheKey')
-    const delta = Number(core.getInput('delta'))
-    const rawTotalDelta = core.getInput('total_delta')
-    const githubClient = github.getOctokit(githubToken)
-    const prNumber = github.context.issue.number
-    const branchNameBase = github.context.payload.pull_request?.base.ref
-    const branchNameHead = github.context.payload.pull_request?.head.ref
-    const useSameComment = JSON.parse(core.getInput('useSameComment'))
-    const commentIdentifier = `<!-- codeCoverageDiffComment -->`
-    const deltaCommentIdentifier = `<!-- codeCoverageDeltaComment -->`
-    let totalDelta = null
-    if (rawTotalDelta !== null) {
-      totalDelta = Number(rawTotalDelta)
-    }
-    let commentId = null
-    execSync(commandToRun)
-    const codeCoverageNew = <CoverageReport>(
-      JSON.parse(fs.readFileSync('coverage-summary.json').toString())
-    )
-    execSync('/usr/bin/git fetch --quiet')
-    execSync('/usr/bin/git stash --quiet')
-    execSync(`/usr/bin/git checkout --quiet --force ${branchNameBase}`)
-    if (afterSwitchCacheKey) {
-      try {
-        const cacheKey = afterSwitchCacheKey
-        const cachePaths = ['~/.npm', '**/*/node_modules', 'node_modules'] // Specify the paths to cache
-
-        // Attempt to restore the cache
-        const restoredCacheKey = await cache.restoreCache(cachePaths, cacheKey)
-        if (restoredCacheKey) {
-          core.info(`Cache restored from key: ${restoredCacheKey}`)
-        } else {
-          core.info('Cache not found, a new cache will be created.')
+    try {
+        const repoName = github.context.repo.repo;
+        const repoOwner = github.context.repo.owner;
+        const commitSha = github.context.sha;
+        const githubToken = core.getInput("accessToken");
+        const fullCoverage = JSON.parse(core.getInput("fullCoverageDiff"));
+        const commandToRun = core.getInput("runCommand");
+        const commandAfterSwitch = core.getInput("afterSwitchCommand");
+        const delta = Number(core.getInput("delta"));
+        const rawTotalDelta = core.getInput("total_delta");
+        const githubClient = github.getOctokit(githubToken);
+        const prNumber = github.context.issue.number;
+        const branchNameBase = github.context.payload.pull_request?.base.ref;
+        const branchNameHead = github.context.payload.pull_request?.head.ref;
+        const useSameComment = JSON.parse(core.getInput("useSameComment"));
+        const commentIdentifier = `<!-- codeCoverageDiffComment -->`;
+        const deltaCommentIdentifier = `<!-- codeCoverageDeltaComment -->`;
+        let totalDelta = null;
+        if (rawTotalDelta !== null) {
+            totalDelta = Number(rawTotalDelta);
         }
+        let commentId = null;
+        execSync(commandToRun);
+        const codeCoverageNew = <CoverageReport>JSON.parse(fs.readFileSync("coverage-summary.json").toString());
+        execSync("/usr/bin/git fetch --quiet");
+        execSync("/usr/bin/git stash --quiet");
+        execSync(`/usr/bin/git checkout --quiet --force ${branchNameBase}`);
+        if (commandAfterSwitch) {
+            execSync(commandAfterSwitch);
+        }
+        execSync(commandToRun);
+        const codeCoverageOld = <CoverageReport>JSON.parse(fs.readFileSync("coverage-summary.json").toString());
+        const currentDirectory = execSync("pwd")
+            .toString()
+            .trim();
+        const diffChecker: DiffChecker = new DiffChecker(codeCoverageNew, codeCoverageOld);
+        let messageToPost = `## Test coverage results :test_tube: \n
+    Code coverage diff between base branch:${branchNameBase} and head branch: ${branchNameHead} \n\n`;
+        const coverageDetails = diffChecker.getCoverageDetails(!fullCoverage, `${currentDirectory}/`);
+        if (coverageDetails.length === 0) {
+            messageToPost = "No changes to code coverage between the base branch and the head branch";
+        } else {
+            messageToPost +=
+                "Status | File | % Stmts | % Branch | % Funcs | % Lines \n -----|-----|---------|----------|---------|------ \n";
+            messageToPost += coverageDetails.join("\n");
+        }
+        messageToPost = `${commentIdentifier}\nCommit SHA:${commitSha}\n${messageToPost}`;
+        if (useSameComment) {
+            commentId = await findComment(githubClient, repoName, repoOwner, prNumber, commentIdentifier);
+        }
+        await createOrUpdateComment(commentId, githubClient, repoOwner, repoName, messageToPost, prNumber);
 
-        // Your custom action logic here
-
-        // Save the cache
-        // const cacheId = await cache.saveCache(cachePaths, cacheKey)
-        // core.info(`Cache saved with key: ${cacheKey} and id: ${cacheId}`)
-      } catch (error) {
-        core.setFailed(error.message)
-      }
-
-      execSync(commandAfterSwitch)
+        // check if the test coverage is falling below delta/tolerance.
+        if (diffChecker.checkIfTestCoverageFallsBelowDelta(delta, totalDelta)) {
+            if (useSameComment) {
+                commentId = await findComment(githubClient, repoName, repoOwner, prNumber, deltaCommentIdentifier);
+            }
+            messageToPost = `Current PR reduces the test coverage percentage by ${delta} for some tests`;
+            messageToPost = `${deltaCommentIdentifier}\nCommit SHA:${commitSha}\n${messageToPost}`;
+            await createOrUpdateComment(commentId, githubClient, repoOwner, repoName, messageToPost, prNumber);
+            throw Error(messageToPost);
+        }
+    } catch (error) {
+        core.setFailed(error);
     }
-    execSync(commandToRun)
-    const codeCoverageOld = <CoverageReport>(
-      JSON.parse(fs.readFileSync('coverage-summary.json').toString())
-    )
-    const currentDirectory = execSync('pwd')
-      .toString()
-      .trim()
-    const diffChecker: DiffChecker = new DiffChecker(
-      codeCoverageNew,
-      codeCoverageOld
-    )
-    let messageToPost = `## Test coverage results :test_tube: \n
-    Code coverage diff between base branch:${branchNameBase} and head branch: ${branchNameHead} \n\n`
-    const coverageDetails = diffChecker.getCoverageDetails(
-      !fullCoverage,
-      `${currentDirectory}/`
-    )
-    if (coverageDetails.length === 0) {
-      messageToPost =
-        'No changes to code coverage between the base branch and the head branch'
-    } else {
-      messageToPost +=
-        'Status | File | % Stmts | % Branch | % Funcs | % Lines \n -----|-----|---------|----------|---------|------ \n'
-      messageToPost += coverageDetails.join('\n')
-    }
-    messageToPost = `${commentIdentifier}\nCommit SHA:${commitSha}\n${messageToPost}`
-    if (useSameComment) {
-      commentId = await findComment(
-        githubClient,
-        repoName,
-        repoOwner,
-        prNumber,
-        commentIdentifier
-      )
-    }
-    await createOrUpdateComment(
-      commentId,
-      githubClient,
-      repoOwner,
-      repoName,
-      messageToPost,
-      prNumber
-    )
-
-    // check if the test coverage is falling below delta/tolerance.
-    if (diffChecker.checkIfTestCoverageFallsBelowDelta(delta, totalDelta)) {
-      if (useSameComment) {
-        commentId = await findComment(
-          githubClient,
-          repoName,
-          repoOwner,
-          prNumber,
-          deltaCommentIdentifier
-        )
-      }
-      messageToPost = `Current PR reduces the test coverage percentage by ${delta} for some tests`
-      messageToPost = `${deltaCommentIdentifier}\nCommit SHA:${commitSha}\n${messageToPost}`
-      await createOrUpdateComment(
-        commentId,
-        githubClient,
-        repoOwner,
-        repoName,
-        messageToPost,
-        prNumber
-      )
-      throw Error(messageToPost)
-    }
-  } catch (error) {
-    core.setFailed(error)
-  }
 }
 
 async function createOrUpdateComment(
-  commentId: number | null,
-  githubClient: {[x: string]: any} & {[x: string]: any} & Octokit &
-    RestEndpointMethods & {paginate: PaginateInterface},
-  repoOwner: string,
-  repoName: string,
-  messageToPost: string,
-  prNumber: number
+    commentId: number | null,
+    githubClient: { [x: string]: any } & { [x: string]: any } & Octokit &
+        RestEndpointMethods & { paginate: PaginateInterface },
+    repoOwner: string,
+    repoName: string,
+    messageToPost: string,
+    prNumber: number,
 ): Promise<void> {
-  if (commentId) {
-    await githubClient.issues.updateComment({
-      owner: repoOwner,
-      repo: repoName,
-      comment_id: commentId,
-      body: messageToPost
-    })
-  } else {
-    await githubClient.issues.createComment({
-      repo: repoName,
-      owner: repoOwner,
-      body: messageToPost,
-      issue_number: prNumber
-    })
-  }
+    if (commentId) {
+        await githubClient.issues.updateComment({
+            owner: repoOwner,
+            repo: repoName,
+            comment_id: commentId,
+            body: messageToPost,
+        });
+    } else {
+        await githubClient.issues.createComment({
+            repo: repoName,
+            owner: repoOwner,
+            body: messageToPost,
+            issue_number: prNumber,
+        });
+    }
 }
 
 async function findComment(
-  githubClient: {[x: string]: any} & {[x: string]: any} & Octokit &
-    RestEndpointMethods & {paginate: PaginateInterface},
-  repoName: string,
-  repoOwner: string,
-  prNumber: number,
-  identifier: string
+    githubClient: { [x: string]: any } & { [x: string]: any } & Octokit &
+        RestEndpointMethods & { paginate: PaginateInterface },
+    repoName: string,
+    repoOwner: string,
+    prNumber: number,
+    identifier: string,
 ): Promise<number> {
-  const comments = await githubClient.issues.listComments({
-    owner: repoOwner,
-    repo: repoName,
-    issue_number: prNumber
-  })
+    const comments = await githubClient.issues.listComments({
+        owner: repoOwner,
+        repo: repoName,
+        issue_number: prNumber,
+    });
 
-  for (const comment of comments.data) {
-    if (comment.body.startsWith(identifier)) {
-      return comment.id
+    for (const comment of comments.data) {
+        if (comment.body.startsWith(identifier)) {
+            return comment.id;
+        }
     }
-  }
-  return 0
+    return 0;
 }
 
-run()
+run();
